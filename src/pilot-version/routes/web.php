dca42f138cf0df287ddccf43f6845fd740bb461a
fuzzy-yusuke <youth_k0228@outlook.com>
2023-11-02 22:59:58 +0900

    #4 ルーティング設定

--- src/pilot-version/routes/web.php
+++ src/pilot-version/routes/web.php
@@ -34,3 +34,32 @@ Route::get(
     '/system/dashboard',
     [App\Http\Controllers\DashboardSystemController::class, 'dashboardsystem']
 )->name('system.dashboardsystem');
+
+/*
+テナントユーザー系ページ
+*/
+// テナントユーザー用ダッシュボード
+Route::get(
+    '/dashboard',
+    [App\Http\Controllers\DashboardController::class, 'dashboard']
+)->name('tenant.dashboard');
+// 自動車マスタ
+Route::get(
+    '/car',
+    [App\Http\Controllers\CarController::class, 'car']
+)->name('tenant.car');
+// 所属マスタ
+Route::get(
+    '/departments',
+    [App\Http\Controllers\DepartmentsController::class, 'departments']
+)->name('tenant.departments');
+// 運転日報
+Route::get(
+    '/drivinglogs',
+    [App\Http\Controllers\DrivingLogsController::class, 'drivinglogs']
+)->name('tenant.drivinglogs');
+// PDFデータ取り込み
+Route::post(
+    '/pdfinput',
+    [App\Http\Controllers\PdfInputController::class, 'pdfinput']
+)->name('tenant.pdfinput');
