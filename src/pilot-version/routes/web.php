d313b6de354a4a05f485b7571a8a59d0096d5b33
fuzzy-yusuke <youth_k0228@outlook.com>
2023-11-02 22:28:48 +0900

    #4 システム管理者用のルーティング設定

--- src/pilot-version/routes/web.php
+++ src/pilot-version/routes/web.php
@@ -20,3 +20,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/*
+システム管理者系ページ
+*/
+// システム管理者用ログイン
+Route::get(
+    '/system/login',
+    [App\Http\Controllers\LoginExController::class, 'loginex']
+)->name('system.loginex');
+// システム管理者用ダッシュボード
+Route::get(
+    '/system/dashboard',
+    [App\Http\Controllers\DashboardSystemController::class, 'dashboardsystem']
+)->name('system.dashboardsystem');
