96b9f58ef4e6ab0b23145eefbbef0e4419bc3d89
fuzzy-yusuke <youth_k0228@outlook.com>
2023-11-05 11:38:09 +0900

    #5 m_userテーブル作成

---
+++ src/pilot-version/database/migrations/2014_10_12_000000_create_m_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_users', function (Blueprint $table) {
+            $table->id('tenant_id');
+            $table->string('user_id', 10);
+            $table->string('user_name');
+            $table->string('email')->unique();
+            $table->string('password_hash');
+            $table->rememberToken();
+            $table->string('department_id', 10);
+            $table->boolean('role')->nullable();
+            $table->timestamp('start_date');
+            $table->timestamp('end_date')->nullable();
+            $table->timestamps();
+            $table->string('updated_by');
+            $table->string('update_program');
+            $table->integer('version')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_users');
+    }
+};
