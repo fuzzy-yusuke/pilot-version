8022d91b068cc2e880159cddf13c2d595b98cd77
fuzzy-yusuke <youth_k0228@outlook.com>
2023-11-07 22:25:22 +0900

    #5 マイグレーションファイルとモデルファイル作成

---
+++ src/pilot-version/app/Models/T_Activity_Logs.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class T_Activity_Logs extends Model
+{
+    use HasFactory;
+}
---
+++ src/pilot-version/app/Models/T_Driving_Logs.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class T_Driving_Logs extends Model
+{
+    use HasFactory;
+}
