e3ac5ecd08df57043d6f953335199c4c95a44498
fuzzy-yusuke <youth_k0228@outlook.com>
2024-01-07 09:44:22 +0900

    タイトル修正

--- src/pilot-version/resources/views/layouts/app.blade.php
+++ src/pilot-version/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'パイロット版') }}</title>
+    <title>@yield('title')</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
