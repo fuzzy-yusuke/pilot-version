0b4e8c2b8cfd415b320d725bd3aeb511bc4eea48
fuzzy-yusuke <youth_k0228@outlook.com>
2024-01-10 09:29:41 +0900

    #4 社員マスタ関連のルーティング追加

--- src/pilot-version/routes/m_user.php
+++ src/pilot-version/routes/m_user.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,6 +11,11 @@ Auth::routes();
 
 // テナントユーザー用ダッシュボード
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('tenant.dashboard');
+//社員マスタ
+Route::get('/user', [App\Http\Controllers\UserMasterController::class, 'index'])->name('tenant.user');
+Route::post('/user/create', [App\Http\Controllers\UserMasterController::class, 'create']);
+Route::post('/user/update', [App\Http\Controllers\UserMasterController::class, 'update']);
+Route::post('/user/delete', [App\Http\Controllers\UserMasterController::class, 'delete']);
 // 自動車マスタ
 Route::get('/car', [App\Http\Controllers\CarController::class, 'index'])->name('tenant.car');
 Route::post('/car/create', [App\Http\Controllers\CarController::class, 'create']);
@@ -21,6 +25,7 @@ Route::post('/car/delete', [App\Http\Controllers\CarController::class, 'delete']
 Route::get('/department', [App\Http\Controllers\DepartmentController::class, 'index'])->name('tenant.department');
 Route::post('/department/create', [App\Http\Controllers\DepartmentController::class, 'create']);
 Route::post('/department/update', [App\Http\Controllers\DepartmentController::class, 'update']);
+Route::post('/department/delete', [App\Http\Controllers\DepartmentController::class, 'delete']);
 // // 運転日報
 // Route::get(
 //     '/drivinglogs',
